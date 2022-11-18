68c7003410cf6d7422262ccab6cd73b544489c23
nvk13sam <nvk13@yandex.ru>
2022-11-18 21:47:57 +0500

    add lesson6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,39 @@
+﻿// УРОК 6. Принципы и антипаттерны
+//
+// Об именовании переменных, классов и других сущностей в языке С# можно почитать в документации:
+// https://learn.microsoft.com/ru-ru/dotnet/csharp/fundamentals/coding-style/identifier-names
+// https://learn.microsoft.com/ru-ru/dotnet/csharp/fundamentals/coding-style/coding-conventions
+
+//***************************************************************
+//1. Антипаттерны - магические числа или константы
+// string caption = "Intensive C# Demo text";
+// int screenWidthPosition = (Console.WindowWidth - caption.Length) / 2;
+// int screenHeightPosition = Console.WindowHeight / 2; 
+
+// void DrawText(string caption, int  screenWidthPosition, int  screenHeightPosition);
+
+// DrawText(
+//    text: caption,
+// 	left: screenWidthPosition,
+// 	top: screenHeightPosition   
+// 	);
+//**************************************************************************************
+// Пример 1. Вывод корней квадратного уравнения (округление double до 2-х знаков)
+// double a = 4, b = -48, c = 60;
+// var d = b * b - 4 * a * c;
+// double x1 = (- b + Math.Sqrt(d)) / (2 * a);
+// double x2 = (- b - Math.Sqrt(d)) / (2 * a);
+// Console.WriteLine($"x1 =" + Math.Round(x1,2));
+// Console.WriteLine($"x2 =" + Math.Round(x2,2));
+// // *************************************************************************************
+// 2. Способ дискреминант через метод
+
+// public Roots Solve(double a, double b, double c)
+// {
+// 	var d = b * b - 4 * a * c;
+// 	double x1 = (- b + Math.Sqrt(d)) / (2 * a);
+// 	double x2 = (- b - Math.Sqrt(d)) / (2 * a);
+// 	return new Roots {X1 = x1, X2 = x2};
+// }
+//***********************************************************************************
+// Программа рисует случайные символы на консоли
