98bdd0758b9fa0792213e70c2d2a22385d9ad766
nvk13sam <nvk13@yandex.ru>
2022-11-18 17:52:53 +0500

    add sort array

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -81,15 +81,95 @@
 // string res = Method41(10, "Пусть будет мир! " + "\n"); // "\n" - перенос на новую строку
 // Console.WriteLine(res);
 
-//*****************************************************************************
-// 1. Выводим на экран таблицу умножения (интерполяция строк)
+//*****************************************************************************************
+// 1. Выводим на экран таблицу умножения (интерполяция строк), табуляция и перенос строк
 
-for (int i = 2; i <= 10; i++)
+// for (int i = 2; i <= 10; i++)
+// {
+// 	for (int j = 2; j <= 10; j++)
+// 	{
+// 		Console.Write($"{i} x {j} = {i * j}" + "\t", "  ");
+// 	}
+// 	Console.WriteLine();
+// }
+
+//****************************************************************************************
+// 2. Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы “к” заменить большими “К”, а большие “С” заменить
+// маленькими “с”.
+// Ясна ли задача? аргументы - это старые символы, которые надо заменить, используя метод
+// string text = "– Я думаю, – сказал князь улыбаясь, – что "
+//                + "ежели бы вас послали вместо нашего милого Винценгероде," 
+//                + " Вы бы взяли приступом согласие прусского короля. "
+//                + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// //string s = "qwerty"
+// //            012345
+// // s[3] = r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+//                string result = String.Empty;
+//                int length = text.Length; //Запомнить длину строки, qwerty=6
+//                for (int i = 0; i < length; i++)
+//                {
+//                              if(text[i] == oldValue) result = result + $"{newValue}";
+//                              else result = result + $"{text[i]}";// имя аргумента - text 
+                             
+//                }
+//                return result;
+
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+
+// Console.WriteLine();
+
+// newText = Replace(newText, 'е', 'Е');
+// Console.WriteLine(newText);
+
+// Console.WriteLine();
+
+// newText = Replace(newText, 'В', 'в');
+// Console.WriteLine(newText);
+
+//********************************************************************************
+//3. Сортируем массив от min до max (методом выбора)
+
+int[] array = { 1, 2, 9, 8, 6, 7, 3, 5, 1, 48, 0, 32, 81, 15, 94, 193, 12, 12, 12, 58, 75 };
+
+void PrintArray(int[] array)
 {
-	for (int j = 2; j <= 10; j++)
-	{
-		Console.Write($"{i} x {j} = {i * j}" + "\t", "  ");
-	}
-	Console.WriteLine();
+              
+      int count = array.Length;
+
+    	for (int i = 0; i < count; i++)
+      {
+            Console.Write($"{array[i]} ");
+      }
+      Console.WriteLine();               
 }
+void SelectionSort(int[] array)
+{
+      for (int i = 0; i < array.Length - 1; i++)
+      {
+            int minPosition = i;
+
+            for (int j = i +1; j < array.Length; j++)
+            {
+                  if(array[j] < array[minPosition]) minPosition = j;   
+            }               
+
+            int temporary = array[i];// обмен местами, переливаем стаканы
+            array[i] = array[minPosition];
+            array[minPosition] = temporary;
+      }
+
+}
+
+PrintArray(array);
+Console.WriteLine();
+SelectionSort(array);
+PrintArray(array);
+
 
